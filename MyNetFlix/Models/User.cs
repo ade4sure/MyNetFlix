74881b4fca302f02aba05f896ca214fca321828e
Adebanjo A.P <ade4sure@gmail.com>
2024-02-07 16:29:53 +0000

    Login Feature

---
+++ MyNetFlix/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MyNetFlix.Models
+{
+    public class User
+    {
+        public required string email { get; set; }
+        public required string password { get; set; }
+        
+    }
+}
