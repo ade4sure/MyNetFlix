975f12ab108ba3cc7466fa60de0bd69ee89839e4
Adebanjo A.P <ade4sure@gmail.com>
2024-02-08 17:21:16 +0000

    VideoPlayer completed

--- MyNetFlix/Controllers/HomeController.cs
+++ MyNetFlix/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MyNetFlix.Models;
 using System.Diagnostics;
+using System.Drawing;
 
 namespace MyNetFlix.Controllers
 {
@@ -16,6 +17,36 @@ namespace MyNetFlix.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult UserHome(string category)
+        {
+            // Create a list of pets.
+            List<Movie> MoviesList =
+            new List<Movie>{ new Movie { Title = "Maami", Category = "Yoruba Movies", Filename="maami.mp4", Producer = "Funke Akindele", Year = 2017 },
+                       new Movie { Title = "Knuckles", Category = "Cartoon", Filename="knuckle.mp4", Producer = "Paramount Plus", Year = 2022 },
+                       new Movie { Title = "Lal Salaam", Category = "Indian Movies", Filename="salaam.mp4", Producer = "Various", Year = 2020 },
+                       new Movie { Title = "Moana", Category = "Cartoon", Filename="moana.mp4", Producer = "Disney Plus", Year = 2024 },
+                       new Movie { Title = "Ayinla Omo Wura", Category = "Yoruba Movies", Filename="ayinla.mp4", Producer = "Ayinla", Year = 2018 },
+                       new Movie { Title = "Kangun Kangun", Category = "Mainframe Movies", Filename="kangun.mp4", Producer = "Tunde Kilani", Year = 2016 },
+                       new Movie { Title = "Saworo Ide (Brass Bell)", Category = "Mainframe Movies", Filename="saworo.mp4", Producer = "Tunde Kilani", Year = 2020 },
+                       };
+
+            var retModel = new MoviesDTO();
+            var userMovies = new List<Movie>();
+            if (category != null)
+            {
+                userMovies = MoviesList.Where(m => m.Category == category).ToList();
+            }
+
+            retModel.Movies = userMovies;
+            retModel.Categories = MoviesList.OrderBy(o => o.Category)
+                                            .GroupBy(mov => mov.Category)
+                                            .Select(g => g.Key)
+                                            .ToList();
+
+
+            return View(retModel);
         }
 
         public IActionResult Privacy()
--- MyNetFlix/Models/User.cs
+++ MyNetFlix/Models/User.cs
@@ -2,8 +2,23 @@
 {
     public class User
     {
-        public required string email { get; set; }
-        public required string password { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
         
+    }
+    
+    public class Movie
+    {
+        public required string Title { get; set; }
+        public required string Producer { get; set; }
+        public required string Category { get; set; }
+        public required string Filename { get; set; }
+        public required int Year { get; set; }
+    }
+
+    public  class MoviesDTO
+    {
+        public IEnumerable<Movie> Movies { get; set; }
+        public List<string> Categories { get; set; }
     }
 }
